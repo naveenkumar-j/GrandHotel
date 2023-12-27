58f336987c3622abe3e7684e82d1c52628164402
Naveenkumar.J <naveenkumar.j@powerschool.com>
2023-12-27 17:16:37 +0530

    Added full date

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-function getCurrentYear() {
+function getCurrentDate() {
     const currentDate = new Date();
-    return currentDate.getFullYear();
-}
-
-// Call the getCurrentYear function
-const currentYear = getCurrentYear();
+    const day = currentDate.getDate();
+    const month = currentDate.getMonth() + 1; // Months are zero-based, so we add 1
+    const year = currentDate.getFullYear();
+  
+    // Pad single-digit day and month with leading zero if needed
+    const formattedDay = day < 10 ? `0${day}` : day;
+    const formattedMonth = month < 10 ? `0${month}` : month;
+  
+    // Create the formatted date string
+    const formattedDate = `${formattedDay}-${formattedMonth}-${year}`;
+  
+    return formattedDate;
+  }
+  
+  // Call the getCurrentDate function
+  const currentDate = getCurrentDate();
